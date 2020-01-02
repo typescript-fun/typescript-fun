0fad459349c7e34819fe23f9a36c19ae012cefd3
Daniel Steigerwald <daniel@steigerwald.cz>
2020-01-02 04:55:47 +0100

    Add Heading1, Heading2 styles

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -2,13 +2,16 @@ import { NextPage } from 'next';
 import React from 'react';
 import { Text } from 'react-native';
 import { Page } from '../components/Page';
+import { useTheme } from '../hooks/useTheme';
 
 const Index: NextPage = () => {
+  const theme = useTheme();
   return (
     <Page title="TypeScript.fun - Typed Functional Programming in Typescript Training">
-      {/* TODO: Header */}
-      <Text accessibilityRole="header">Header</Text>
-      <Text>text</Text>
+      <Text style={theme.heading1}>
+        Typed Functional Programming in Typescript
+      </Text>
+      <Text style={theme.heading2}>Why</Text>
     </Page>
   );
 };
--- packages/web/themes/theme.ts
+++ packages/web/themes/theme.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-// That's all we need for a "design system".
-// We don't even need constants. Just find and replace what has to be changed.
+// That's all we need for a "design system". We don't even need constants.
+// Just find and replace what has to be changed. It's type-safe.
 // For example, `16` or `padding: 16`.
-//  Note we can compose styles via `StyleSheet.compose`.
+
+// MozOsxFontSmoothing?: string,
+// WebkitFontSmoothing?: string
 
 export const theme = StyleSheet.create({
   container: {
@@ -19,5 +21,23 @@ export const theme = StyleSheet.create({
   logo: {
     height: 45,
     width: 300,
+  },
+  heading1: {
+    color: 'rgb(26, 32, 44)',
+    fontFamily:
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+    fontSize: 24,
+    fontWeight: '700',
+    lineHeight: 30,
+    marginBottom: 32,
+  },
+  heading2: {
+    color: 'rgb(26, 32, 44)',
+    fontFamily:
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+    fontSize: 20,
+    fontWeight: '700',
+    lineHeight: 25,
+    marginBottom: 16,
   },
 });
