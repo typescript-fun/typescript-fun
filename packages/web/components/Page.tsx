732a74d8ac82dd90ef5149fbb455ed7a2efd1192
Daniel Steigerwald <daniel@steigerwald.cz>
2020-01-02 04:19:30 +0100

    Add PageHeader, Logo

--- packages/web/components/Page.tsx
+++ packages/web/components/Page.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
 import Head from 'next/head';
+import React, { FC } from 'react';
+import { View } from 'react-native';
 import { useTheme } from '../hooks/useTheme';
-import { View, Image } from 'react-native';
-// import { Logo } from './Logo';
+import { Logo } from './Logo';
 
 export const Page: FC<{ title: string }> = ({ title, children }) => {
   const theme = useTheme();
@@ -12,10 +12,9 @@ export const Page: FC<{ title: string }> = ({ title, children }) => {
         <title>{title}</title>
       </Head>
       <View style={theme.container}>
-        <Image
-          style={{ width: 300, height: 45 }}
-          source={{ uri: '/logo.svg' }}
-        />
+        <View style={theme.pageHeader}>
+          <Logo />
+        </View>
         {children}
       </View>
     </>
--- packages/web/themes/theme.ts
+++ packages/web/themes/theme.ts
@@ -2,15 +2,22 @@ import { StyleSheet } from 'react-native';
 
 // That's all we need for a "design system".
 // We don't even need constants. Just find and replace what has to be changed.
-// For example, `16` or `padding: 16`. I suppose this is the best DX.
-// Note we can compose styles via `StyleSheet.compose`.
+// For example, `16` or `padding: 16`.
+//  Note we can compose styles via `StyleSheet.compose`.
 
 export const theme = StyleSheet.create({
   container: {
-    backgroundColor: '#cccccc',
     marginHorizontal: 'auto',
     maxWidth: 740,
     padding: 16,
     width: '100%',
+  },
+  pageHeader: {
+    marginBottom: 48,
+    marginTop: 32,
+  },
+  logo: {
+    height: 45,
+    width: 300,
   },
 });
