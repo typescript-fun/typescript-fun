f965ee7bdfea597d740ba9929ddd74bd2fae6bbe
Daniel Steigerwald <daniel@steigerwald.cz>
2020-01-02 23:29:13 +0100

    Close #1

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -4,6 +4,9 @@ module.exports = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web',
+      // Tree shaking.
+      // https://github.com/gcanti/fp-ts/issues/1044#issue-536939300
+      'fp-ts/lib': 'fp-ts/es6',
     };
     config.resolve.extensions.push('.web.js', '.web.ts', '.web.tsx');
     return config;
