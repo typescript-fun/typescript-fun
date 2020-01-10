ff5def0ee7611734f1735abe0e75280d7da24e41
Daniel Steigerwald <daniel@steigerwald.cz>
2020-01-10 02:28:52 +0100

    Update signup.tsx

--- packages/web/pages/examples/signup.tsx
+++ packages/web/pages/examples/signup.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Text } from 'react-native';
 import { Page } from '../../components/Page';
 import { useTheme } from '../../hooks/useTheme';
-import { Link } from '../../components/Link';
+// import { Link } from '../../components/Link';
 
 const Index: NextPage = () => {
   const theme = useTheme();
   return (
     <Page title="Sign Up - Examples - TypeScript.fun">
       <Text style={theme.heading1}>Sign Up</Text>
-      <Text>foo</Text>
+      <Text>TODO</Text>
     </Page>
   );
 };
